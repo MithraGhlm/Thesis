86575af5a2645adf2359546f1084fd3ac679568d
Mithra <gholm1@bfh.ch>
2024-07-28 16:10:43 +0200

    rviz and twist mux are added to the launch file.

--- turtlebot3_gazebo/launch/mybot_world.launch.py
+++ turtlebot3_gazebo/launch/mybot_world.launch.py
@@ -14,12 +14,14 @@ def generate_launch_description():
     package_name = 'turtlebot3_gazebo' 
     launch_file_dir = os.path.join(get_package_share_directory(package_name), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    my_package_dir = get_package_share_directory(package_name)
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    use_ros2_control = LaunchConfiguration('use_ros2_control', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-8.5')
     y_pose = LaunchConfiguration('y_pose', default='2.0')
 
-   ## TODO make Ur world and replace it here
+   ## replace Ur world here to make it appear
     world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
@@ -42,16 +44,16 @@ def generate_launch_description():
         launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )
 
-    ## TODO replace your robot state publisher launch file
+    ## robot state publisher launch file
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(launch_file_dir, 'mybot_robot_state_publisher.launch.py')
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control':use_ros2_control}.items()
     )
 
 
-    ## TODO replace the launch file to spawn Ur own robot here
+    ## launch file to spawn Ur own robot
     spawn_mybot_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(launch_file_dir, 'spawn_mybot.launch.py')
@@ -61,6 +63,25 @@ def generate_launch_description():
             'y_pose': y_pose
         }.items()
     )
+
+    if use_sim_time == 'true':
+        _use_sim_time = f"use_sim_time:={True}"
+    else:
+        _use_sim_time = f"use_sim_time:={False}"
+        
+    rviz_laucher= Node(package='rviz2',
+             executable='rviz2',
+             arguments=['-d', os.path.join(my_package_dir, 'config', 'main.rviz'), '--ros-args', '-p', _use_sim_time])
+    
+
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
 
 
     diff_drive_spawner = Node(
@@ -83,6 +104,8 @@ def generate_launch_description():
     ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_mybot_cmd)
     # -------
+    ld.add_action(rviz_laucher)
+    ld.add_action(twist_mux)
     ld.add_action(diff_drive_spawner)
     ld.add_action(joint_broad_spawner)
 
