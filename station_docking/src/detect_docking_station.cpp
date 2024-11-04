7e0bbcb76140ee4833c90899f6a39d001b8db95c
Mithra <gholm1@bfh.ch>
2024-11-04 09:19:57 +0100

    detects and marks 2 lines with a certaine degree. Still needs some limitation for expected result.

--- station_docking/src/detect_docking_station.cpp
+++ station_docking/src/detect_docking_station.cpp
@@ -40,7 +40,7 @@ public:
     //scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>("processed_scan", 10);
 
     // Publisher for intermediate PointCloud2 data (visualization or debug)
-    pointcloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("processed_PC2", 10);
+    pointcloud2_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("processed_PC2", 10);
 
     line_marker_pub_=this->create_publisher<visualization_msgs::msg::Marker>("Marker", 10);
   }
@@ -65,11 +65,12 @@ private:
     sor.setLeafSize(0.01f, 0.01f, 0.01f); // leaf size of 1cm
     sor.filter(cloud_filtered);
 
-    // Converting from PCLPointCloud2 to PCLPointCloud (for better PCL processing)
-    //?pcl::PointCloud<pcl::PointXYZ> cloud_pc;
-    //?pcl::fromPCLPointCloud2(cloud_filtered, cloud_pc);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pc(new pcl::PointCloud<pcl::PointXYZ>());
+
+    // Converting from PCLPointCloud2 to PCLPointCloud (for better PCL processing)
     pcl::fromPCLPointCloud2(cloud_filtered, *cloud_pc);
+     
+    //std::cout << "Number of points in the filtered point cloud: " << cloud_pc->size() << std::endl;
 
     //==========================================>
 
@@ -77,7 +78,7 @@ private:
     seg.setOptimizeCoefficients(true);
     seg.setModelType(pcl::SACMODEL_LINE);
     seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setDistanceThreshold(0.05); // Adjust based on point cloud
+    seg.setDistanceThreshold(0.004); // Adjust based on point cloud
     //seg.setMaxIterations(1000);
 
 
@@ -85,42 +86,48 @@ private:
     pcl::ModelCoefficients::Ptr line_coefficients(new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr line_inliers(new pcl::PointIndices);
     pcl::ExtractIndices<pcl::PointXYZ> extract;
-    std::vector<pcl::ModelCoefficients::Ptr> line_coeffs;
+    std::vector<pcl::ModelCoefficients::Ptr> coeffs;
+    std::vector<pcl::PointIndices::Ptr> inliers;
 
-    while(true){
-        seg.setInputCloud(cloud_pc);
-        seg.segment(*line_inliers, *line_coefficients);
+    // std::cout << "Entering while loop ((((((( " << std::endl;
+    // while(true){
 
-        // Check if any line was detected at all
-        if (line_inliers->indices.empty()) {
-            break;
-        }
-        //std::cout << "line_coefficients: " << *line_coefficients << std::endl;
-        line_coeffs.push_back(line_coefficients);
+    //     seg.setInputCloud(cloud_pc);
+    //     seg.segment(*line_inliers, *line_coefficients);
+
+    //     // Check if any line was detected at all
+    //     if (line_inliers->indices.empty()) {
+    //         break;
+    //     }
+    //     std::cout << "line_coefficients: " << *line_coefficients << std::endl;
+    //     coeffs.push_back(line_coefficients);
         
-        // Extract the line points from the pointcloud data
-        extract.setInputCloud(cloud_pc);
-        extract.setIndices(line_inliers);
-        extract.setNegative(true);
-        extract.filter(*cloud_pc);
-    }
-    std::cout << "number of lines found is: " << line_coeffs.size() << std::endl;
+    //     // Extract the line points from the pointcloud data
+    //     extract.setInputCloud(cloud_pc);
+    //     extract.setIndices(line_inliers);
+    //     extract.setNegative(true);
+    //     extract.filter(*cloud_pc);
+    // }
+    // std::cout << "number of lines found is: " << coeffs.size() << std::endl;
+    // std::cout << "Exiting while loop )))))))) " << std::endl;
 
-
-    /*
+    // /***************************************************
     // Detect the 1st line using RANSAC
     pcl::ModelCoefficients::Ptr line1_coefficients(new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr line1_inliers(new pcl::PointIndices);
     seg.setInputCloud(cloud_pc);
     seg.segment(*line1_inliers, *line1_coefficients);
+    coeffs.push_back(line1_coefficients);
+    inliers.push_back(line1_inliers);
     // std::cout << "line1_coefficients: " << *line1_coefficients << std::endl;
+    //publishLine(line1_coefficients, line1_inliers, 1.0, 0.0, 0.0);
     
 
-    // Extract line 1 points
-    pcl::ExtractIndices<pcl::PointXYZ> extract; 
+    // Extract line 1 points from data
     extract.setInputCloud(cloud_pc);
     extract.setIndices(line1_inliers);
     extract.setNegative(true);
+    extract.setKeepOrganized (true);
     extract.filter(*cloud_pc);
 
     // Detect the 2nd line using RANSAC
@@ -128,16 +135,16 @@ private:
     pcl::PointIndices::Ptr line2_inliers(new pcl::PointIndices);
     seg.setInputCloud(cloud_pc);
     seg.segment(*line2_inliers, *line2_coefficients);
-    */
+    coeffs.push_back(line2_coefficients);
+    inliers.push_back(line2_inliers);
+    //publishLine(line2_coefficients, line2_inliers, 0.0, 1.0, 0.0);
+
+    // // publish both lines
+    // publishCrossMarker(line1_coefficients, line2_coefficients, line1_inliers, line2_inliers);
+    // ****************************************************/
 
     // Calculate intersection point (approximation for now)
-    geometry_msgs::msg::Point intersection_point;
-    // Implement a simple 2D intersection calculation using line coefficients here
-
-    findCrossShape(line_coeffs);
-
-
-
+    findCrossShape(coeffs, inliers);
 
     //==========================================>
 
@@ -170,7 +177,7 @@ private:
     output_cloud.header = cloud_msg.header;  // Keep the original header
 
     // publishing PointCloud2 result
-    pointcloud_publisher_->publish(output_cloud);
+    pointcloud2_publisher_->publish(output_cloud);
 
     // Converting filtered PointCloud2 back to LaserScan (is needed?)
     // implement this conversion if the result in LaserScan format is needed
@@ -182,19 +189,19 @@ private:
   } // scan_cb
 
 
-  void findCrossShape(const std::vector<pcl::ModelCoefficients::Ptr> &line_coeffs)
+  void findCrossShape(const std::vector<pcl::ModelCoefficients::Ptr> &coeffs, const std::vector<pcl::PointIndices::Ptr> &inliers)
   {
-    // Define a target angle for "cross" detection (e.g., 90 degrees, in radians)
-    const float target_angle = pcl::deg2rad(90.0f); 
-    const float angle_tolerance = pcl::deg2rad(10.0f); // Acceptable tolerance range
+    // Defining a target angle for "cross" detection (e.g., 90 degrees, in radians)
+    const float target_angle = pcl::deg2rad(60.0f); 
+    const float angle_tolerance = pcl::deg2rad(5.0f); // Acceptable tolerance range
 
-    for (size_t i = 0; i < line_coeffs.size(); ++i)
+    for (size_t i = 0; i < coeffs.size(); ++i)
     {
-      for (size_t j = i + 1; j < line_coeffs.size(); ++j)
+      for (size_t j = i + 1; j < coeffs.size(); ++j)
       {
         // Extract direction vectors for lines i and j
-        Eigen::Vector2f dir_i(line_coeffs[i]->values[3], line_coeffs[i]->values[4]);
-        Eigen::Vector2f dir_j(line_coeffs[j]->values[3], line_coeffs[j]->values[4]);
+        Eigen::Vector2f dir_i(coeffs[i]->values[3], coeffs[i]->values[4]);
+        Eigen::Vector2f dir_j(coeffs[j]->values[3], coeffs[j]->values[4]);
 
         // Calculate the angle between the two direction vectors
         float angle = std::acos(dir_i.dot(dir_j) / (dir_i.norm() * dir_j.norm()));
@@ -202,20 +209,20 @@ private:
         // Check if the angle is close to the target angle for a cross shape
         if (std::abs(angle - target_angle) < angle_tolerance)
         {
-          RCLCPP_INFO(this->get_logger(), "Cross detected between lines %ld and %ld", i, j);
-          publishCrossMarker(line_coeffs[i], line_coeffs[j]);
+          //RCLCPP_INFO(this->get_logger(), "Cross detected between lines %ld and %ld", i, j);
+          publishCrossMarker(coeffs[i], coeffs[j], inliers[i], inliers[j]);
           return;
         }
       }
     }
   }
 
-  void publishCrossMarker(const pcl::ModelCoefficients::Ptr &line1, const pcl::ModelCoefficients::Ptr &line2)
+  void publishCrossMarker(const pcl::ModelCoefficients::Ptr &line1, const pcl::ModelCoefficients::Ptr &line2, const pcl::PointIndices::Ptr &inliers1, const pcl::PointIndices::Ptr &inliers2)
   {
     // Create markers for visualization in Rviz
     visualization_msgs::msg::Marker marker1, marker2;
 
-    marker1.header.frame_id = marker2.header.frame_id = "lidar_frame";
+    marker1.header.frame_id = marker2.header.frame_id = "laser_frame";
     marker1.header.stamp = marker2.header.stamp = this->get_clock()->now();
     marker1.ns = "line1";
     marker2.ns = "line2";
@@ -225,7 +232,10 @@ private:
     marker2.type = visualization_msgs::msg::Marker::LINE_STRIP;
     marker1.action = visualization_msgs::msg::Marker::ADD;
     marker2.action = visualization_msgs::msg::Marker::ADD;
-    marker1.scale.x = marker2.scale.x = 0.05; // Set line width
+    // Set line width
+    marker1.scale.x = marker2.scale.x = 0.03; 
+    marker1.scale.y = marker2.scale.y = 0.03; 
+    marker1.scale.z = marker2.scale.z = 0.0;
 
     // Set color
     marker1.color.r = 1.0;
@@ -239,22 +249,59 @@ private:
 
     // Define points for line 1 and line 2
     geometry_msgs::msg::Point p1_start, p1_end, p2_start, p2_end;
-    p1_start.x = line1->values[0]; p1_start.y = line1->values[1]; p1_start.z = line1->values[2];
+    p1_start.x = line1->values[0]; p1_start.y = line1->values[1]; p1_start.z = 0.0;
     p1_end.x = p1_start.x + line1->values[3]; 
     p1_end.y = p1_start.y + line1->values[4]; 
 
-    p2_start.x = line2->values[0]; p2_start.y = line2->values[1]; p2_start.z = line2->values[2];
+    p2_start.x = line2->values[0]; p2_start.y = line2->values[1]; p2_start.z = 0.0;
     p2_end.x = p2_start.x + line2->values[3]; 
     p2_end.y = p2_start.y + line2->values[4]; 
 
     marker1.points.push_back(p1_start); marker1.points.push_back(p1_end);
     marker2.points.push_back(p2_start); marker2.points.push_back(p2_end);
 
-    // Publish markers
-    line_marker_pub_->publish(marker1);
-    line_marker_pub_->publish(marker2);
+    // // Publish markers
+    // if(inliers1->indices.size() > 40){
+       line_marker_pub_->publish(marker1);
+    // }
+    // if(inliers2->indices.size() > 40){
+       line_marker_pub_->publish(marker2);
+    // }
   }
 
+  // publish only one line
+  void publishLine(const pcl::ModelCoefficients::Ptr &line, const pcl::PointIndices::Ptr &inliers, const float r, const float g, const float b){
+    visualization_msgs::msg::Marker marker;
+    marker.header.frame_id = "laser_frame";
+    marker.header.stamp = this->get_clock()->now();
+    marker.ns = "line";
+    marker.id = 0;
+    marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.scale.x = 0.03; marker.scale.y = 0.03; marker.scale.z = 0.0;
+
+    //color
+    marker.color.r = r;
+    marker.color.g = g;
+    marker.color.b = b;
+    marker.color.a = 1.0;
+
+    // Define points for line 1
+    geometry_msgs::msg::Point p_start, p_end;
+    p_start.x = line->values[0]; p_start.y = line->values[1]; //p_start.z = 0.0;
+    p_end.x = p_start.x + 0.5* line->values[3]; 
+    p_end.y = p_start.y + 0.5* line->values[4];
+
+    marker.points.push_back(p_start); marker.points.push_back(p_end);
+
+    // setting threshold for the minimum number of inliers in a line
+    if(inliers->indices.size() > 40){
+        line_marker_pub_->publish(marker);
+    }
+    
+
+    std::cout << "number of inliers: " << inliers->indices.size() << std::endl;
+  }
 
 
   // LaserScan to PointCloud2 projector
@@ -262,8 +309,8 @@ private:
 
   // Subscriber and publishers
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_publisher_;
-  //rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud2_publisher_;
+
   //publisher for visualization markers
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr line_marker_pub_;
 };
@@ -291,7 +338,34 @@ int main(int argc, char **argv)
   rclcpp::init(argc, argv);
   auto node = std::make_shared<LidarPclProcessor>();
   rclcpp::spin(node);
+
+  // Create a lambda function to spin the node in a separate thread
+  auto spin_thread = std::thread([&node]() {
+    rclcpp::spin(node);
+  });
+
+  // Create a Rate object with 10 Hz
+  rclcpp::Rate rate(100);
+
+  try {
+        while (rclcpp::ok()) {
+            std::cout << "Help me body, you are my only hope" << std::endl;
+            rate.sleep();
+        }
+    } catch (const std::exception &e) {
+        // Handle exceptions if needed
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+
+  // Shutdown the ROS 2 client library
   rclcpp::shutdown();
+
+  // Join the spin thread to clean up
+  if (spin_thread.joinable()) {
+      spin_thread.join();
+  }
+
   return 0;
 }
 
