1ae4adbffd503582b07f72a128bc6a0a43e6308f
Mithra <gholm1@bfh.ch>
2024-12-03 10:39:54 +0100

    in waypoint_following, using Actions instead of publishing a Topic containing waypoints.

--- waypoint_navigation/src/waypoint_manager.cpp
+++ waypoint_navigation/src/waypoint_manager.cpp
@@ -1,4 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
+#include "rclcpp/executor.hpp"
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/joy.hpp>
 #include <tf2_ros/transform_listener.h>
@@ -6,24 +7,34 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <nav2_msgs/action/navigate_to_pose.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
+
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "nav2_msgs/action/follow_waypoints.hpp"
+//#include "nav2_core/waypoint_task_executor.hpp"
 #include <vector>
 #include <memory>
 
 
-
+using namespace std::chrono_literals;
 
 class WaypointNavigator : public rclcpp::Node {
 public:
     using NavigateToPose = nav2_msgs::action::NavigateToPose;
+    using FollowWaypoints = nav2_msgs::action::FollowWaypoints;
     using GoalHandleNavigateToPose = rclcpp_action::ClientGoalHandle<NavigateToPose>;
+    using WaypointFollowerGoalHandle = rclcpp_action::ClientGoalHandle<nav2_msgs::action::FollowWaypoints>;
 
     WaypointNavigator()
-        : Node("waypoint_recorder"), tf_buffer_(get_clock()), tf_listener_(tf_buffer_) {
+        : Node("waypoint_navigator"), tf_buffer_(get_clock()), tf_listener_(tf_buffer_), server_timeout_(100) {
         joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10, std::bind(&WaypointNavigator::joystick_cb, this, std::placeholders::_1));
 
-        // action client for navigation
-        nav_client_ = rclcpp_action::create_client<NavigateToPose>(this, "navigate_to_pose");
+        // clients
+        navigation_action_client_ = rclcpp_action::create_client<NavigateToPose>(this, "navigate_to_pose");
+        waypoint_follower_action_client_ = rclcpp_action::create_client<FollowWaypoints>(this,"follow_waypoints");
+
+        
 
         RCLCPP_INFO(this->get_logger(), "WaypointNavigator node started.");
     }
@@ -45,7 +56,8 @@ private:
         if (msg->buttons[3] == 1 && (now - last_button_3_press_) > debounce_duration_) {
             last_button_3_press_ = now;
             RCLCPP_INFO(this->get_logger(), "Starting waypoint navigation...");
-            followWaypoints();
+            //followWaypoints();
+            startWaypointFollowing(waypoints_);
         }
     }
 
@@ -69,9 +81,10 @@ private:
         }
     }
 
+#if 0
     // Updating followWaypoints() to send waypoints as goals to the action server and receive feedback about the robot's progress and final status
     void followWaypoints() {
-        if (!nav_client_->wait_for_action_server(std::chrono::seconds(10))) {
+        if (!navigation_action_client_->wait_for_action_server(std::chrono::seconds(5))) {
             RCLCPP_ERROR(this->get_logger(), "Navigation action server not available.");
             return;
         }
@@ -80,28 +93,73 @@ private:
             RCLCPP_INFO(this->get_logger(), "Navigating to waypoint at (%.2f, %.2f)", 
                     waypoint.pose.position.x, waypoint.pose.position.y);
 
-        auto goal_msg = NavigateToPose::Goal();
-            goal_msg.pose = waypoint;
+            //auto navigation_goal_ = NavigateToPose::Goal();
+            //navigation_goal_.pose = waypoint;
 
+            // configure callbacks for monitoring the progress of a goal sent to action server
             auto send_goal_options = rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
             send_goal_options.feedback_callback =
                 std::bind(&WaypointNavigator::feedback_cb, this, std::placeholders::_1, std::placeholders::_2);
             send_goal_options.result_callback =
                 std::bind(&WaypointNavigator::result_cb, this, std::placeholders::_1);
 
-            auto goal_handle_future = nav_client_->async_send_goal(goal_msg, send_goal_options);
+            // send_goal_options.goal_response_callback =
+            // std::bind(&WaypointFollower::goalResponse_cb, this, std::placeholders::_1);
+            
 
-            // Wait for the robot to finish navigating to this waypoint
-            rclcpp::spin_until_future_complete(this->get_node_base_interface(), goal_handle_future);
+            auto future_goal_handle_ = navigation_action_client_->async_send_goal(navigation_goal_, send_goal_options);
+            //auto result_future = future_goal_handle_.get()->async_get_result();
+            rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_goal_handle_); // result_future?
         }
 
-
         RCLCPP_INFO(this->get_logger(), "Finished following all waypoints.");
-    }
+    } // followWaypoints()
+#endif
 
-    void feedback_cb(
-        GoalHandleNavigateToPose::SharedPtr,
-        const std::shared_ptr<const NavigateToPose::Feedback> feedback) {
+    void startWaypointFollowing(std::vector<geometry_msgs::msg::PoseStamped> poses){
+        auto is_action_server_ready = waypoint_follower_action_client_->wait_for_action_server(std::chrono::seconds(5));
+        if (!is_action_server_ready) {
+            RCLCPP_ERROR(this->get_logger(), "follow_waypoints action server is not available."
+                " Is the initial pose set?");
+            return;
+        }
+        std::cout << "In the startWaypointFollowing function." << std::endl;
+        // Send the goal poses
+        waypoint_follower_goal_.poses = poses;
+        
+
+        RCLCPP_DEBUG(this->get_logger(), "Sending a path of %zu waypoints:", waypoint_follower_goal_.poses.size());
+        for (auto waypoint : waypoint_follower_goal_.poses) {
+        RCLCPP_DEBUG(this->get_logger(), "\t(%lf, %lf)", waypoint.pose.position.x, waypoint.pose.position.y);
+        }
+
+        // Enable result awareness by providing an empty lambda function
+        auto send_goal_options = rclcpp_action::Client<FollowWaypoints>::SendGoalOptions();
+        send_goal_options.result_callback = [this](auto) {waypoint_follower_goal_handle_.reset();
+        };
+        send_goal_options.feedback_callback = [this](WaypointFollowerGoalHandle::SharedPtr /*goal_handle*/,
+        const std::shared_ptr<const nav2_msgs::action::FollowWaypoints::Feedback> feedback) {feedback->current_waypoint;
+        };
+
+        auto future_goal_handle = waypoint_follower_action_client_->async_send_goal(waypoint_follower_goal_, send_goal_options);
+
+        //if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), future_goal_handle, server_timeout_) != rclcpp::FutureReturnCode::SUCCESS) //why?
+        // {
+        //     RCLCPP_ERROR(this->get_logger(), "Send goal call failed");
+        //     return;
+        // }
+
+        // Get the goal handle and save so that we can check on completion in the timer callback
+        waypoint_follower_goal_handle_ = future_goal_handle.get();
+        if (!waypoint_follower_goal_handle_) {
+            RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+            return;
+        }
+
+    } // startWaypointFollowing()
+
+#if 0
+    void feedback_cb( GoalHandleNavigateToPose::SharedPtr, const std::shared_ptr<const NavigateToPose::Feedback> feedback) {
         RCLCPP_INFO(this->get_logger(), "Feedback: Robot is at position: [%.2f, %.2f]",
                     feedback->current_pose.pose.position.x, feedback->current_pose.pose.position.y);
     }
@@ -122,9 +180,27 @@ private:
             break;
         }
     }
+#endif
+    // // handle the server's response to the goal request
+    // void goalResponse_cb(const rclcpp_action::ClientGoalHandle<ClientT>::SharedPtr & goal){
+    //     if (!goal) {
+    //         RCLCPP_ERROR(get_logger(), "navigate_to_pose action client failed to send goal to server.");
+    //         current_goal_status_.status = ActionStatus::FAILED;
+    //     }
+    // }
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
-    rclcpp_action::Client<NavigateToPose>::SharedPtr nav_client_;
+    rclcpp_action::Client<NavigateToPose>::SharedPtr navigation_action_client_;
+    rclcpp_action::Client<FollowWaypoints>::SharedPtr waypoint_follower_action_client_;
+
+    // waypoint_follower_goal_ = FollowWaypoints::Goal(); //590 why?
+    // nav_through_poses_goal_ = NavigateThroughPoses::Goal(); //591 why?
+    NavigateToPose::Goal navigation_goal_{};
+    FollowWaypoints::Goal waypoint_follower_goal_{};
+
+    // Goal-related state
+    WaypointFollowerGoalHandle::SharedPtr waypoint_follower_goal_handle_;
+
 
     /* tf2_ros::Buffer Stores transformations and provides lookup functions to get the current position and orientation of the robot.
      The tf_buffer_ object keeps a history of recent transforms between different frames. It's used to track the transformation from the map frame to
@@ -139,19 +215,33 @@ private:
      */
     tf2_ros::TransformListener tf_listener_;
 
+    // Timeout value when waiting for action servers to respnd
+    std::chrono::milliseconds server_timeout_;
+
     std::vector<geometry_msgs::msg::PoseStamped> waypoints_;
 
     // Debouncing
     rclcpp::Time last_button_0_press_ {this->now()};  // last press time button 0
     rclcpp::Time last_button_3_press_ {this->now()};  // last press time button 3
     rclcpp::Duration debounce_duration_ {std::chrono::milliseconds(500)};  // time tolerance (ms)
+
+
+// protected:
+//     rclcpp::executors::SingleThreadedExecutor executor_;
+
+
 };
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<WaypointNavigator>();
 
-    rclcpp::spin(node);
+    auto node = std::make_shared<WaypointNavigator>();
+    rclcpp::executors::SingleThreadedExecutor executor_;
+
+    //rclcpp::spin(node);
+    executor_.add_node(node);
+    executor_.spin();
+
     rclcpp::shutdown();
     return 0;
 }
